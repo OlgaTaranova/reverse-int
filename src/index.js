94bc730800f5568aca3900580480d310539ed310
Olga Taranova <olka_olkataranova@mail.ru>
2020-10-06 23:32:22 +0300

    feat: implement function 'reverse'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let result = n.toString().split('');
+    if (result[0] == '-') {
+      result.shift();
+    }
+    
+    return +result.reverse().join('');
 }
